5ecc11f31d973d51aa96af3e27fa00e6ca8b8aa7
Long G <long.w.guan@gmail.com>
2023-10-23 17:46:16 -0700

    Added react-img-mapper and created first shape on map

---
+++ frontend/src/StevensPassMap.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ImageMapper from "react-img-mapper";
+import stevens_pass_map from "./assets/stevens_pass_map.jpg";
+import stevens_pass_runs from "./assets/stevens_pass_runs.json";
+
+function StevensPassMap(props) {
+  const MAP = {
+    name: "stevenspassmap",
+    areas: stevens_pass_runs,
+  };
+
+  return <ImageMapper natural="true" src={stevens_pass_map} map={MAP} />;
+}
+
+export default StevensPassMap;

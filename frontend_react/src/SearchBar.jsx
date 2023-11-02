4b418b94f4248e280dcef70a0404c4f5a53e7ad7
Long G <long.w.guan@gmail.com>
2023-11-01 17:13:53 -0700

    Added styling and drop down option for search bar

--- frontend_react/src/SearchBar.jsx
+++ frontend_react/src/SearchBar.jsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
+import stevens_pass_runs from "./assets/stevens_pass_runs.json";
+
+const notFocusedStyle = {
+  borderRadius: "20px",
+  maxWidth: "200px",
+  paddingTop: "10px",
+  paddingBottom: "10px",
+  backgroundColor: "#fff",
+};
+
+const focusedStyle = {
+  borderTopLeftRadius: "20px",
+  borderTopRightRadius: "20px",
+  maxWidth: "200px",
+  paddingTop: "10px",
+  paddingBottom: "10px",
+  backgroundColor: "#fff",
+};
+
 function SearchBar(props) {
+  const [style, setStyle] = useState(notFocusedStyle);
+  // used to track focus on drop down select so the onBlur from the input
+  // doesn't close it when trying to select an option
+  const [selectClick, setSelectClick] = useState(false);
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit} style={{ alignSelf: "start" }}>
       <label
         htmlFor="default-search"
         className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
@@ -8,7 +38,10 @@ function SearchBar(props) {
         Search Trails
       </label>
       <div className="relative">
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <div
+          style={{ height: "42px" }}
+          className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+        >
           <svg
             className="w-4 h-4 text-gray-500 dark:text-gray-400"
             aria-hidden="true"
@@ -29,18 +62,60 @@ function SearchBar(props) {
           type="search"
           id="default-search"
           className="block w-full p-1 pl-10 text-sm border focus:outline-none shadow-lg"
-          style={{
-            borderRadius: "20px",
-            maxWidth: "200px",
-            paddingTop: "10px",
-            paddingBottom: "10px",
-            backgroundColor: "#fff",
-          }}
+          style={style}
           placeholder="Search..."
           required
           onChange={(e) => props.setRunSelection(e.target.value)}
           value={props.runSelection}
+          onFocus={() => {
+            setStyle(focusedStyle);
+            setShowDropDown(true);
+          }}
+          onBlur={() => {
+            if (selectClick !== true) {
+              setStyle(notFocusedStyle);
+              setShowDropDown(false);
+            }
+          }}
         />
+        {showDropDown === true ? (
+          <select
+            className="focus:outline-none"
+            size="4"
+            style={{
+              width: "100%",
+              boxShadow:
+                "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
+              borderBottomLeftRadius: "20px",
+              borderBottomRightRadius: "20px",
+            }}
+            onClick={() => {
+              setShowDropDown(false);
+              setStyle(notFocusedStyle);
+            }}
+            onMouseEnter={() => setSelectClick(true)}
+            onMouseLeave={() => setSelectClick(false)}
+            onChange={(e) => {
+              props.setRunSelection(e.target.value);
+            }}
+            name="search"
+          >
+            {stevens_pass_runs.map((run) => (
+              <option
+                className="text-sm searchbar-option"
+                style={{
+                  height: "42px",
+                  textAlign: "center",
+                  paddingTop: "10px",
+                }}
+                key={run.name}
+                value={run.title}
+              >
+                {run.title}
+              </option>
+            ))}
+          </select>
+        ) : null}
       </div>
     </form>
   );
